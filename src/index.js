50301afb1e3a5922e54057418aa8c64bbb2a3a25
Kotaro <swdevstudio@gmail.com>
2020-08-12 11:41:56 +0300

    Так убрал не нужное

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 // Подключение плагинов и библиотек
-import 'owl.carousel'
-import 'owl.carousel/dist/assets/owl.carousel.css';
+// import 'owl.carousel'
+// import 'owl.carousel/dist/assets/owl.carousel.css';
 
 
 
